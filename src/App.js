7ee3575b29482e611a1deef20afa34e9ce2d22cf
Mateus Oliveira <mateusluisoliveira@gmail.com>
2020-04-08 17:50:56 -0300

    Resolvendo desafio ReactJS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories,setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Novo Repositório ${Date.now()}`,
+      url: 'http://github.com/MLDOliveira/SemanaOmniStack11',
+      techs: ["NodeJS","ReactJS"]
+    });
+
+    const repository = response.data;
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
     // TODO
+    try {
+      const response = await api.delete(`repositories/${id}`);
+
+      setRepositories(repositories.filter(repo => repo.id !== id));
+    } catch (error) {
+      alert("Erro ao deletar registro");
+      console.log(error);
+    }
+  
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {
+          repositories.map(repo => (
+            <li key={repo.id}>
+              {repo.title}
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
+              </button>
+            </li>
+          ))
+        }
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
